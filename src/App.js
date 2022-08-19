a8438b05e0c3ab4b384ced9b1cc31e8b57401852
MaxChabanov <84992334+MaxChabanov@users.noreply.github.com>
2022-08-19 19:07:16 +0300

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="counter-container">
+      <div className="counter-background">
+        <p className="temp-counter">
+          <span className="counter-number" id="counter-number">
+            26
+          </span>
+          <span className="degree-symbol">°</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
