f49b695b4ae08de4c43b395ed53ce19b815a6664
MaxChabanov <84992334+MaxChabanov@users.noreply.github.com>
2022-08-21 14:54:59 +0300

    finally, the styles are done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 function App() {
   return (
     <div className="counter-container">
+      <div className="header">
+        <h1>Temperature Control</h1>
+      </div>
       <p className="temp-counter">
         <p className="counter-number" id="counter-number">
           26
@@ -11,9 +14,11 @@ function App() {
       </p>
 
       <div className="room-container">
+        <button className="btn-operation">+</button>
         <p id="room-name" className="room-name">
           Kitchen
         </p>
+        <button className="btn-operation">-</button>
       </div>
     </div>
   );
